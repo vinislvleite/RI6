a0f97a5835a92b5a57f4a486e840a0f89352c437
Vinícius Leite <163305926+vinislvleite@users.noreply.github.com>
2025-09-18 18:08:34 -0300

    Delete src directory
    Vou mudar o nome da main

--- src/divisão.ts
+++
@@ -1,7 +0,0 @@
-import Calculo from "./calculo"
-
-export default class Divisao extends Calculo {
-    public calcular(n1:number,n2:number):number {
-        return n1 / n2
-    }
-}
--- src/soma.ts
+++
@@ -1,7 +0,0 @@
-import Calculo from "./calculo"
-
-export default class Soma extends Calculo {
-    public calcular(n1:number,n2:number):number {
-        return n1 + n1
-    }
-}
