7c7cbb4c4c35a3b911c89e2951dbabb1b5532fcf
Vinícius Leite <163305926+vinislvleite@users.noreply.github.com>
2025-09-18 18:24:14 -0300

    Delete src directory
    Arrumando erros

--- src/multiplicação.ts
+++
@@ -1,7 +0,0 @@
-import Calculo from "./calculo"
-
-export default class Multiplicacao extends Calculo {
-    public calcular(n1:number,n2:number):number {
-        return n1 * n2
-    }
-}
--- src/radiciação.ts
+++
@@ -1,7 +0,0 @@
-import Calculo from "./calculo"
-
-export default class Raiz extends Calculo {
-    public calcular(numero: number, indice: number): number {
-        return numero ** (1 / indice)
-    }
-}
